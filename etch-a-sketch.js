af86ff14c6945ee1365ea0e3f36b6724561e7612
xop14 <112329100+xop14@users.noreply.github.com>
2022-11-01 19:25:00 +0900

    Add undo function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,6 +4,8 @@ const sizeSlider = document.querySelector("#size-slider");
 const sizeSliderDisplay = document.querySelector("#size-slider-display");
 const clearBtn = document.querySelector("#clear-btn");
 const randomBtn = document.querySelector("#random-btn");
+const undoBtn = document.querySelector("#undo-btn");
+const canvasHistory = [];
 
 const body = document.body;
 
@@ -15,7 +17,10 @@ let colors = ["red", "orange", "yellow", "limegreen", "green", "blue", "skyblue"
 let mouseDown = 0;
 let brushSize = 3;
 let isRandomColors = false;
-const currentCanvas = [];
+
+let xPos = null;
+let yPos = null;
+
 
 
 // detect when mouse is down
@@ -33,6 +38,7 @@ createGrid(gridSize, pixelSize);
 
 // create pixel grid
 function createGrid(gridSize, pixelSize) {
+    const currentCanvas = [];
     //reset current grid
     grid.innerHTML = "";
 
@@ -45,6 +51,7 @@ function createGrid(gridSize, pixelSize) {
             pixel.style.width = pixelSize;
             pixel.style.height = pixelSize;
             pixel.style.backgroundColor = "white";
+            pixel.setAttribute('id', `i${i}j${j}`);
 
             grid.appendChild(pixel);
             //pixel.textContent = i + " , " + j;
@@ -58,6 +65,9 @@ function createGrid(gridSize, pixelSize) {
                     currentCanvas[i][j] = currentColor;
                 }
                 pixel.classList.add("pixel-hover");
+                xPos = j;
+                yPos = i;
+                //console.log("x: " + j + " y: " + i);
             });
             pixel.addEventListener("mousedown", () => {
                 if (isRandomColors) {
@@ -69,12 +79,52 @@ function createGrid(gridSize, pixelSize) {
             pixel.addEventListener("mouseout", () => {
                 pixel.classList.remove("pixel-hover");
             });
+            pixel.addEventListener("mouseup", () => {
+                saveCanvasUndo(currentCanvas);
+            });
             
             currentCanvasRow.push("white");
         }
         currentCanvas.push(currentCanvasRow);
     }
+    saveCanvasUndo(currentCanvas);
 }
+
+// save array to history
+function saveCanvasUndo(canvasArray) {
+    const newCanvasUndo = [];
+    for (let i = 0; i < canvasArray.length; i++) {
+        let newCanvasUndoRow = [];
+        for (let j = 0; j < canvasArray.length; j++) {
+            newCanvasUndoRow[j] = canvasArray[i][j];
+        }
+        newCanvasUndo.push(newCanvasUndoRow);
+    }
+    canvasHistory.push(newCanvasUndo);
+}
+
+// undo
+undoBtn.addEventListener("click", undo);
+
+function undo() {
+    console.log("UNDO PRESSED");
+    if (canvasHistory.length - 1 <= 0) {
+        console.log("NO HISTORY");
+        return;
+    }
+    console.log("HISTORY FOUND");
+    
+    const lastUndo = canvasHistory[canvasHistory.length - 2];
+    
+    for (let i = 0; i < lastUndo.length; i++) {
+        for (let j = 0; j < lastUndo.length; j++) {
+            document.querySelector(`#i${i}j${j}`).style.backgroundColor = lastUndo[i][j];
+        }
+    }
+
+    canvasHistory.pop();
+}
+
 
 
 
@@ -91,6 +141,7 @@ colors.forEach((color) => {
     });
 });
 
+updateColorCss("red");
 
 // update css
 function updateColorCss(currentColor) {
@@ -124,12 +175,12 @@ randomBtn.addEventListener("click", () => {
     if (isRandomColors == false) {
         isRandomColors = true;
         randomBtn.style.backgroundColor = "yellow";
-        randomBtn.textContent = "Random mode ON";
+        randomBtn.textContent = "Random color mode ON";
     }
     else {
         isRandomColors = false;
         randomBtn.style.backgroundColor = "#f0f0f0";
-        randomBtn.textContent = "Random mode OFF";
+        randomBtn.textContent = "Random color mode OFF";
     }
 });
 
@@ -138,3 +189,7 @@ function randomColor() {
     currentColor = colors[i];
     updateColorCss(currentColor);
 }  
+
+
+// opacity function
+
